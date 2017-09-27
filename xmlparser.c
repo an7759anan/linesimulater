3c15046b2a175c20fd61c657d6048a7f40357b2e
An7759anan <an7759anan@rtural.ru>
2017-09-27 07:29:40 +0500

    Additionaly checkings ... not finished ...

--- xmlparser.c
+++ xmlparser.c
@@ -117,6 +117,54 @@ void parse_ntp(){
   sscanf_P(p1,PSTR(".%hu</ntp>"),&v); ntp[3]=v;
 }
 
+/*
+            <step event="line_shorting" time_offset="0"/>
+            <step event="earth_shorting" time_offset="4900"/>
+            <step event="norm" time_offset="35900"/>
+            <step time_offset="40000" cause="crushed" limit="0.45" max_overflow_value="1.56"/>
+*/
+
+uint8_t parseNextStepTagXMLformatError;
+
+uint8_t notFillerCharsOnly(char *p0, char *p1){
+   uint8_t result=0;
+   for(p0++;p0<p1;p0++)
+      if (!(*p0==' '||*p0=='\n'||*p0=='\r'||*p0=='\t')) {
+	     result=1;
+		 break;
+	  }
+   return result;
+}
+
+uint16_t countChar(char *buf, char c){
+   uint16_t count = 0;
+   char *p = buf;
+   while (*p)
+      if (*p++ == c) count++;
+   return count;
+}
+
+/*
+        </wareData>
+    </parameters>
+</root>*/
+
+uint8_t checkConfigureXML_2(char *p){
+   char *wareData1=strstr_P(p,PSTR("</wareData>"));
+   char *parameters1=strstr_P(p,PSTR("</parameters>"));
+   char *root1=strstr_P(p,PSTR("</root>"));
+
+   if (parameters1==0 || wareData1==0 || root1) return 0;
+
+   if (! (
+       parameters1 > wareData1
+	&& root1 > parameters1
+   )) return 0;
+
+   if ( !(countChar(wareData1, '<') == 3 && countChar(wareData1, '>') == 3)) return 0;
+ 
+   return 1;
+}
 
 char* parseNextStepTag(char* p, uint8_t sNum){
   char* p1=strstr_P(p,PSTR("<step"));
@@ -127,45 +175,69 @@ char* parseNextStepTag(char* p, uint8_t sNum){
   if (p1){
     p2=strstr_P(p1,PSTR("/>"));
 	if (p2){
+       p3=strstr_P(p2+2,PSTR("<step"));
+       if (notFillerCharsOnly(p2+2,p3)){
+          parseNextStepTagXMLformatError=1;
+		  return 0;
+	   }
        *p2=0;
-       logger(p1);logger("\n");
+       if (strstr_P(p1+1,PSTR("<"))){
+          parseNextStepTagXMLformatError=1;
+		  return 0;
+	   }
+       if (strstr_P(p1+1,PSTR(">"))){
+          parseNextStepTagXMLformatError=1;
+		  return 0;
+	   }
        step->impedance=0xFFFF;
        step->attenuation=0xFFFF;
        step->resistance=0xFFFF;
        step->breakage=0;
 
        p3=strstr_P(p1,PSTR("time_offset="));
-       sscanf_P(p3,PSTR("time_offset=\"%lu\""),&step->time_offset);
-
-	   if (strstr_P(p1,PSTR("norm"))){
+	   if (p3==0){
+          parseNextStepTagXMLformatError=1;
+		  return 0;
 	   }
-	   else if (strstr_P(p1,PSTR("line_shorting"))){
+       if(sscanf_P(p3,PSTR("time_offset=\"%lu\""),&step->time_offset)==0){
+          parseNextStepTagXMLformatError=1;
+		  return 0;
+	   }
+
+	   if (strstr_P(p1,PSTR("\"norm\""))){
+	   }
+	   else if (strstr_P(p1,PSTR("\"line_shorting\""))){
           step->impedance=0;
 	   }
-	   else if (strstr_P(p1,PSTR("earth_shorting"))){
+	   else if (strstr_P(p1,PSTR("\"earth_shorting\""))){
           step->resistance=0;
 	   }
-	   else if (strstr_P(p1,PSTR("line_break"))){
+	   else if (strstr_P(p1,PSTR("\"line_break\""))){
           step->breakage=1;
 	   }
-	   else if (strstr_P(p1,PSTR("crushed"))){
+	   else if (strstr_P(p1,PSTR("\"crushed\""))){
           step->breakage=1;
 	   }
-	   else if (strstr_P(p1,PSTR("bended"))){
+	   else if (strstr_P(p1,PSTR("\"bended\""))){
           step->breakage=1;
 	   }
-	   else if (strstr_P(p1,PSTR("overheated"))){
+	   else if (strstr_P(p1,PSTR("\"overheated\""))){
           step->breakage=1;
 	   }
-	   else if (strstr_P(p1,PSTR("inside explosion funnel"))){
+	   else if (strstr_P(p1,PSTR("\"inside explosion funnel\""))){
           step->breakage=1;
 	   }
-	   else if (strstr_P(p1,PSTR("fail command post"))){
+	   else if (strstr_P(p1,PSTR("\"fail command post\""))){
           step->breakage=1;
 	   }
-	   else if (strstr_P(p1,PSTR("overtension"))){
+	   else if (strstr_P(p1,PSTR("\"overtension\""))){
           step->breakage=1;
 	   }
+	   else {
+	      parseNextStepTagXMLformatError=1;
+		  return 0;// I guess that w5100 buf not be cleared ...
+       }
+       logger(p1);logger("\n");
 	   *p2='/';
 	   return p2+2;
 	} else {// Finish step tag not found
@@ -173,18 +245,12 @@ char* parseNextStepTag(char* p, uint8_t sNum){
 	}
     return 0;
   }
-  else if (strstr_P(p,PSTR("</wareData>"))){
-     return 0;
+//  else if (strstr_P(p,PSTR("</wareData>"))){
+  else if (strstr_P(p,PSTR("</root>"))){
+//     if (checkConfigureXML_2(p)!=1) parseNextStepTagXMLformatError=1;
+	 return 0;
   } else 
      return p;
-}
-
-uint16_t countChar(char *buf, char c){
-   uint16_t count = 0;
-   char *p = buf;
-   while (*p)
-      if (*p++ == c) count++;
-   return count;
 }
 
 uint8_t checkStartXML(){
@@ -215,18 +281,14 @@ uint8_t checkStartXML(){
     || start_time1==0
    ) return 0;
 /*
-   if (strstr_P(root0+1,PSTR("<root>"))) return 0;
-   if (strstr_P(root1+1,PSTR("</root>"))) return 0;
-   if (strstr_P(action0+1,PSTR("<action>"))) return 0;
-   if (strstr_P(action1+1,PSTR("</action>"))) return 0;
-   if (strstr_P(parameters0+1,PSTR("<parameters>"))) return 0;
-   if (strstr_P(parameters1+1,PSTR("</parameters>"))) return 0;
-   if (strstr_P(il_id0+1,PSTR("<il_id>"))) return 0;
-   if (strstr_P(il_id1+1,PSTR("</il_id>"))) return 0;
-   if (strstr_P(exp_id0+1,PSTR("<exp_id>"))) return 0;
-   if (strstr_P(exp_id1+1,PSTR("</exp_id>"))) return 0;
-   if (strstr_P(start_time0+1,PSTR("<start_time>"))) return 0;
-   if (strstr_P(start_time1+1,PSTR("</start_time>"))) return 0;
+<root>
+    <action>start</action>
+    <parameters>
+        <il_id>0</il_id>
+        <exp_id>111</exp_id>
+        <start_time>1505926419</start_time>
+    </parameters>
+</root>
 */
    if (! (
        action0 > root0
@@ -239,6 +301,14 @@ uint8_t checkStartXML(){
    )) return 0;
 
    if ( !(countChar(root0, '<') == 12 && countChar(root0, '>') == 12)) return 0;
+
+   if (notFillerCharsOnly(root0+5,action0)
+    || notFillerCharsOnly(action1+8,parameters0)
+    || notFillerCharsOnly(parameters0+11,il_id0)
+    || notFillerCharsOnly(il_id1+7,exp_id0)
+    || notFillerCharsOnly(exp_id1+8,start_time0)
+    || notFillerCharsOnly(start_time1+12,parameters1)
+    || notFillerCharsOnly(parameters1+12,root1)) return 0;
  
    return 1;
 }
@@ -301,24 +371,9 @@ uint8_t checkSettingsXML(){
    char *ntp0=strstr_P((char *)globalBuf,PSTR("<ntp>"));
    char *ntp1=strstr_P((char *)globalBuf,PSTR("</ntp>"));
 
-   if (root0==0
-    || root1==0
-    || action0==0
-    || action1==0
-    || parameters0==0
-    || parameters1==0
-    || ip0==0
-    || ip1==0
-    || ipport0==0
-    || ipport1==0
-	|| netmask0==0
-	|| netmask1==0
-	|| logger0==0
-	|| logger1==0
-	|| loggerport0==0
-	|| loggerport1==0
-	|| ntp0==0
-	|| ntp1==0
+   if (root0==0  || root1==0  || action0==0     || action1==0     || parameters0==0 || parameters1==0
+    || ip0==0    || ip1==0    || ipport0==0     || ipport1==0	  || netmask0==0    || netmask1==0
+	|| logger0==0|| logger1==0|| loggerport0==0	|| loggerport1==0 || ntp0==0        || ntp1==0
    ) return 0;
 
    if (! (
@@ -334,6 +389,55 @@ uint8_t checkSettingsXML(){
    )) return 0;
 
    if ( !(countChar(root0, '<') == 18 && countChar(root0, '>') == 18)) return 0;
+ 
+   return 1;
+}
+/*
+<root>
+    <action>configure</action>
+    <parameters>
+        <il_id>0</il_id>
+        <exp_id>111</exp_id>
+        <maxTime>60</maxTime>
+        <wareData distance="30">
+*/
+uint8_t checkConfigureXML_1(){
+   char *root0=strstr_P((char *)globalBuf,PSTR("<root>"));
+   char *action0=strstr_P((char *)globalBuf,PSTR("<action>"));
+   char *action1=strstr_P((char *)globalBuf,PSTR("</action>"));
+   char *parameters0=strstr_P((char *)globalBuf,PSTR("<parameters>"));
+   char *il_id0=strstr_P((char *)globalBuf,PSTR("<il_id>"));
+   char *il_id1=strstr_P((char *)globalBuf,PSTR("</il_id>"));
+   char *exp_id0=strstr_P((char *)globalBuf,PSTR("<exp_id>"));
+   char *exp_id1=strstr_P((char *)globalBuf,PSTR("</exp_id>"));
+   char *maxTime0=strstr_P((char *)globalBuf,PSTR("<maxTime>"));
+   char *maxTime1=strstr_P((char *)globalBuf,PSTR("</maxTime>"));
+   char *wareData0=strstr_P((char *)globalBuf,PSTR("<wareData"));
+   char *wareData1=0;
+   if (wareData0) wareData1=strstr_P(wareData0+1,PSTR(">"));
+   char *step0=strstr_P((char *)globalBuf,PSTR("<step"));
+   char *wareData11=strstr_P((char *)globalBuf,PSTR("/>"));
+
+
+   if (root0==0  || action0==0     || action1==0     || parameters0==0 
+    || il_id0==0    || il_id1==0    || exp_id0==0     || exp_id1==0	  || maxTime0==0    || maxTime1==0
+	|| wareData0==0 || wareData1==0 || step0==0
+   ) return 0;
+
+   if (! (
+       action0 > root0
+	&& parameters0 > action1
+	&& il_id0 > parameters0
+	&& exp_id0 > il_id1
+	&& maxTime0 > exp_id1 
+	&& wareData0 > maxTime1
+    && step0 > wareData1
+	&& (wareData11 == 0 || wareData11 > step0)
+   )) return 0;
+
+   *step0=0;
+   if ( !(countChar(root0, '<') == 11 && countChar(root0, '>') == 11)) return 0;
+   *step0='<';
  
    return 1;
 }
@@ -441,6 +545,10 @@ uint16_t xmlParse(uint16_t rsize){
 		   return 0;
 		 }
            logger("Got configure command\n");
+		   if (checkConfigureXML_1()==0){
+              sprintf_P ((char *)globalBuf, fromILtoFA_SPS_bad_XML_format);
+              return 0;
+		   }
            if (parse_il_id()==0 || parse_exp_id()==0 || parse_maxTime()==0 || parse_distance()==0){
               sprintf_P ((char *)globalBuf, fromILtoFA_SPS_start_stop_absent_params,"configure");
               return 0;
@@ -457,10 +565,11 @@ uint16_t xmlParse(uint16_t rsize){
 		      return 0;
 		   }
            controller_stepsNumber=0;
+		   parseNextStepTagXMLformatError=0;
        case 1:// Configure command parsing in progress ...
            pp0=(char *)globalBuf;
 		   pp1=pp0;
-           while (pp1=parseNextStepTag(pp0,controller_stepsNumber++)){
+           while ((pp1=parseNextStepTag(pp0,controller_stepsNumber++)) && parseNextStepTagXMLformatError==0){
 		      if (pp1 == pp0){
                  xmlParseState=1;
                  controller_stepsNumber--;
@@ -470,6 +579,11 @@ uint16_t xmlParse(uint16_t rsize){
               logger("Configure step elapsed\n");
               if(controller_stepsNumber>STEPSBUFSIZE-1) break;
            }
+		   if (parseNextStepTagXMLformatError){
+              sprintf_P ((char *)globalBuf, fromILtoFA_SPS_bad_XML_format);
+              controller_state = CONTROLLER_STATE_CONFIG_ERROR;
+			  return 0;
+		   }
            controller_stepsNumber--;
            controller_il_id=_il_id;
 		   controller_exp_id=_exp_id;
